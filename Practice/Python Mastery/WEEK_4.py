08665071e38d2e0071c15feb4a5695846c332c23
Durvankur4 <durvankurchavan4@gmail.com>
2025-11-22 15:16:16 +0530

    week 4 prob 2 solved

--- Practice/Python Mastery/WEEK_4.py
+++ Practice/Python Mastery/WEEK_4.py
@@ -6,12 +6,45 @@
 # isinstance(x, (int, float))
 # Simple iteration
 
-def my_sum(*numbers):
-    sum = 0
-    for num in numbers:
-        if isinstance(num , (int,float)):
-            sum += num
-    # print(numbers)
-    return sum
+# def my_sum(*numbers):
+#     sum = 0
+#     for num in numbers:
+#         if isinstance(num , (int,float)): 
+#             sum += num
+#     # print(numbers) 
+#     return sum
 
-print(my_sum(1,2,3,"abc"))
+# print(my_sum(1,2,3,"abc"))
+
+
+
+# Problem 2 (Medium) — Function factory using closures
+# Write make_counter(start=0) that returns a function.
+# Each time you call the returned function, it increases the counter by 1 and returns it.
+# Example:
+# c = make_counter(10)
+# c() → 11  
+# c() → 12
+# Topics required
+# Closures
+# Nonlocal variables
+# Function returning a function
+# Step-by-step
+# Define outer function accepting start.
+# Inside, define inner function that modifies start.
+# Use nonlocal so inner function updates outer variable.
+# Return inner function.
+
+
+# def make_counter(start=0) :
+#     count = start
+#     def inner():
+#         nonlocal count
+#         count += 1 
+#         return count
+#     return inner
+
+# c = make_counter(10)
+
+# assert c() == 11
+# assert c() == 12
