7b895f6233e9f0487a5b9196b7d217b591cedb10
Durvankur4 <durvankurchavan4@gmail.com>
2025-11-24 16:48:05 +0530

    week 5 p1, added documentation reference

--- Practice/Python Mastery/WEEK_5.py
+++ Practice/Python Mastery/WEEK_5.py
@@ -5,11 +5,37 @@
 # Iteration over file objects
 # File modes (“r”)
 
-def count_lines(path:str):
-    count = 0
-    with open(path , "r") as f :
-        for _ in f :
-            count += 1
-    return count
+# def count_lines(path:str):
+#     count = 0
+#     with open(path , "r") as f :
+#         for _ in f :
+#             count += 1
+#     return count
 
-print(count_lines('Practice\Python Mastery\WEEK_4.py'))
+# print(count_lines('Practice\Python Mastery\WEEK_4.py'))
+
+
+# Problem 2 (Medium) — Build a tiny module
+# Create two files:
+# mathutils.py
+# def square(n):
+#     return n*n
+# def cube(n):
+#     return n*n*n
+# main.py
+# import mathutils
+# print(mathutils.square(5))
+# print(mathutils.cube(3))
+# Your task:
+# Write a function run_math(path) that automatically imports the module from the given path and runs both functions.
+# Topics required
+# Modules
+# importlib
+# Attribute access via getattr
+
+import mathutils
+print(mathutils.square(5))
+print(mathutils.cube(3))
+
+### too complex for my understanding currently redo
+
