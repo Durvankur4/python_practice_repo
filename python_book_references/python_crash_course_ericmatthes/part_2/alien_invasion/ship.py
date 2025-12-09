fa5b38572b6057dfb8fb108b33d10a4da660e301
Durvankur4 <durvankurchavan4@gmail.com>
2025-12-09 18:01:42 +0530

    incon commit

--- python_book_references/python_crash_course_ericmatthes/part_2/alien_invasion/ship.py
+++ python_book_references/python_crash_course_ericmatthes/part_2/alien_invasion/ship.py
@@ -6,10 +6,11 @@ class Ship:
 
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
-
+        self.settings = ai_game.settings
         self.imgae = pygame.image.load("D:/03_Code\python\python_book_references\python_crash_course_ericmatthes\part_2/alien_invasion\images\ship.bmp")
         self.rect = self.imgae.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
 
          ### movement flag
         self.moving_right = False
@@ -17,12 +18,12 @@ class Ship:
 
     def update(self):
         '''update ships postion'''
-        if self.moving_right :
-            self.rect.x += 1
-        elif self.moving_left :
-            self.rect.x -= 1
+        if self.moving_right and self.rect.right<self.screen_rect.right :
+            self.x += self.settings.ship_speed
+        elif self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
 
-
+        self.rect.x = self.x
 
     
     def blitme(self):
