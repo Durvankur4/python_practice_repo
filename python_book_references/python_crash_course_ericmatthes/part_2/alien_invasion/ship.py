714beb5fe8dc92540324fe6cb80bfa39d015badc
Durvankur4 <durvankurchavan4@gmail.com>
2025-12-09 14:20:01 +0530

    added movement to ship

--- python_book_references/python_crash_course_ericmatthes/part_2/alien_invasion/ship.py
+++ python_book_references/python_crash_course_ericmatthes/part_2/alien_invasion/ship.py
@@ -10,7 +10,23 @@ class Ship:
         self.imgae = pygame.image.load("D:/03_Code\python\python_book_references\python_crash_course_ericmatthes\part_2/alien_invasion\images\ship.bmp")
         self.rect = self.imgae.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
+
+         ### movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        '''update ships postion'''
+        if self.moving_right :
+            self.rect.x += 1
+        elif self.moving_left :
+            self.rect.x -= 1
+
+
+
     
     def blitme(self):
         '''draw the ship'''
         self.screen.blit(self.imgae,self.rect)
+
+    
