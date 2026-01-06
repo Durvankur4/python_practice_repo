300e7438c8ecd354ac8f5a30fe46d82d30143a92
Durvankur4 <durvankurchavan4@gmail.com>
2026-01-06 17:35:01 +0530

    solved problem_217

--- python_questions/neetcode/insert_into_BST.py
+++ python_questions/neetcode/insert_into_BST.py
@@ -10,15 +10,15 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        if not root:
-            return TreeNode(val)
-        if val > root.val:
-            root.right = self.insertIntoBST(root.right, val)
-        else:
-            root.left = self.insertIntoBST(root.left, val)
-        return root
+# class Solution:
+#     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+#         if not root:
+#             return TreeNode(val)
+#         if val > root.val:
+#             root.right = self.insertIntoBST(root.right, val)
+#         else:
+#             root.left = self.insertIntoBST(root.left, val)
+#         return root
 
 
 # time complexity = O(n)
